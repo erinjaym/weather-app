361ae3f8e314ccba8fa5d681f50506cd979fac98
EJM <erin.jay.moyer@gmail.com>
2021-05-10 17:51:01 -0600

    added default location pull

--- weather.js
+++ weather.js
@@ -5,12 +5,19 @@ let weatherInformation = {};
 function grabInput(){
 let citySelection = document.getElementById('city').value;
 getLocationDetails(citySelection);
-
 }
+
+const generateToken = (function (){
+let tokenGeneration = "47715d3c047fdb0a7399443d3242ba9f"
+function weatherApi() {
+    return tokenGeneration;
+}
+return {weatherApi}
+})();
 
 //getLocationDetails("Denver");
 function getLocationDetails (location) {
-fetch (`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=47715d3c047fdb0a7399443d3242ba9f`)
+fetch (`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${generateToken.weatherApi()}`)
 .then(function(response){
     return response.json();
 })
@@ -38,13 +45,22 @@ function populateWeather(){
     conditions.textContent = weatherInformation.weather[802].description;
     console.log(weatherInformation); */
 
-    let actualFeeling = document.getElementById('feels-like'); // might still not pull
+    let actualFeeling = document.getElementById('feels-like'); 
     let whyNoWork = 1.8 * (parseInt(weatherInformation.main.feels_like)-273) + 32;
     actualFeeling.textContent = whyNoWork;
-
-
 }
 
-
+(function getDefaultLocation() {
+    fetch (`https://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&appid=47715d3c047fdb0a7399443d3242ba9f`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+    weatherInformation = response;
+    populateWeather();
+    })
+    .catch ((error) => { console.log(error);});
+    })();
+    
 
 
