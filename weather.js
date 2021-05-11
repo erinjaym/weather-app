d87565a3194bf1016dc632448bacc38174cf85fb
EJM <erin.jay.moyer@gmail.com>
2021-05-10 19:53:29 -0600

    Added styling to page. Added Background pictures. Added a function to change the background picture based upon weather conditions of selected city.

--- weather.js
+++ weather.js
@@ -28,26 +28,51 @@ populateWeather();
 .catch ((error) => { console.log(error);});
 }
 
+
+
+
+
+
 // on submit say hey populate the weather 
 function populateWeather(){
-        let currentLocation = document.getElementById('location');
-        currentLocation.textContent = weatherInformation.name;
+    setBackgroundPic();
+    function setBackgroundPic () {
+        let findSnow = /snow/ig;
+        let findRain = /rain/ig;
+        let findClouds = /cloud/ig;
+        let findClear = /clear/ig;
+
+        let stringToSearch = weatherInformation.weather[0].description;
+
+        if (stringToSearch.match(findSnow)){
+            document.body.style.backgroundImage = "url('./images/snow.jpeg')";
+        }else if(stringToSearch.match(findRain)){
+            document.body.style.backgroundImage = "url('./images/rain.jpeg')";
+        }else if (stringToSearch.match(findClouds)){
+            document.body.style.backgroundImage = "url('./images/clouds.jpeg')";
+        }else if (stringToSearch.match(findClear)){
+            document.body.style.backgroundImage = "url('./images/clear.jpeg')";
+        }
+    };
+
+
+    let currentLocation = document.getElementById('location');
+    currentLocation.textContent = weatherInformation.name;
 
     let currentTemperature = document.getElementById('temperature');
     let tempToFahrenheight = 1.8 * (parseInt(weatherInformation.main.temp)-273) + 32;
-    currentTemperature.textContent = tempToFahrenheight;
+    currentTemperature.textContent = tempToFahrenheight + "°F";
 
     let humidity = document.getElementById('humidity');
-    humidity.textContent = weatherInformation.main.humidity;
+    humidity.textContent = weatherInformation.main.humidity + '%';
 
-/*
-    let conditions = document.getElementById('conditions');
-    conditions.textContent = weatherInformation.weather[802].description;
-    console.log(weatherInformation); */
+    let conditions = document.getElementById('condition');
+    conditions.textContent = weatherInformation.weather[0].description;
+    console.log(weatherInformation);
 
     let actualFeeling = document.getElementById('feels-like'); 
-    let whyNoWork = 1.8 * (parseInt(weatherInformation.main.feels_like)-273) + 32;
-    actualFeeling.textContent = whyNoWork;
+    let feelingToF = 1.8 * (parseInt(weatherInformation.main.feels_like)-273) + 32;
+    actualFeeling.textContent = feelingToF + "°F";
 }
 
 (function getDefaultLocation() {
@@ -62,5 +87,6 @@ function populateWeather(){
     .catch ((error) => { console.log(error);});
     })();
     
+
 
 
